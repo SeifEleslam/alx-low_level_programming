56043cfb610d98a45e24e7cd291a1d897edb6f8d
SeifEleslam <seifgouda98@gmail.com>
2023-08-10 14:45:13 -0700

    task5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*Function that returns fixed string*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+  /*Function that returns fixed string*/
+  puts("\"Programming is like building a multilingual puzzle\n");
+  return (0);
 }
