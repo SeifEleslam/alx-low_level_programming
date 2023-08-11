63a921b8235b58e33e2d52ce507b1f9fdf3c8d09
SeifEleslam <seifgouda98@gmail.com>
2023-08-11 14:59:46 -0700

    task6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program prints a fixed text to the standard error'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char l;
+
+for (l = 0; l <= 9; l++)
+{
+printf("%d", l);
+}
+printf("\n");
+return (0);
+}
