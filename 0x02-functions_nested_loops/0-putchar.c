69fadc13d462599130a2dbe1852461ae0801c0c6
SeifEleslam <seifgouda98@gmail.com>
2023-08-15 16:10:11 -0700

    test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,13 @@
  */
 int main(void)
 {
-    int i = 0;
-    char *string = "_putchar";
-    while (i < (int)strlen(string))
-    {
-        _putchar(string[i]);
-        i++;
-    }
-    _putchar('\n');
-
-    return (0);
+	int i = 0;
+	char *string = "_putchar";
+	while (i < (int)strlen(string)){
+		_putchar(string[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
 
