33b5a3d4fe07b0aa7959b05a028b2d8af81e0ae2
SeifEleslam <seifgouda98@gmail.com>
2023-08-15 16:47:45 -0700

    task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _islower - returns 1 if c is lowercase, else it returns 0
+ * @c: int to be tested
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
+
