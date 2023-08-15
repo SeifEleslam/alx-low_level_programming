71e70f1c20541a1b3fde414beeadc1dc0609ca49
SeifEleslam <seifgouda98@gmail.com>
2023-08-15 16:40:00 -0700

    test

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,11 +4,12 @@
  * print_alphabet_x10 - prints alphabet ten times
  */
 
-void print_alphabet_x10(void){
+void print_alphabet_x10(void)
+{
 	char c;
 	int n;
 
-	for(n = 1; n <=10; n++)
+	for(n = 1; n <= 10; n++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
