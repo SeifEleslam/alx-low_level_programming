2e9d29dd764417bc313ee87a23e6177e465cc25b
SeifEleslam <seifgouda98@gmail.com>
2023-08-16 03:55:28 -0700

    test

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,7 +4,7 @@
 /**
  * num_len - return the length of unsigned long num
  * @num: the number to return its length
- * Returns: the length of num
+ * Return: the length of num
  */
 
 int num_len(unsigned long num)
@@ -29,8 +29,10 @@ int num_len(unsigned long num)
  * fb - prints first limit  numbers of the fibonacci sequence
  * @f1: the num berfore previous number
  * @f2: the previous number
+ * @f1o: the small part of f1
+ * @f2o: the small part of f2
  * @limit: the limit for this fb function
- * Return: 0
+ * Return: void
  */
 
 void fb(unsigned long f1, unsigned long f1o, unsigned long f2, unsigned long f2o, int limit)
@@ -38,31 +40,18 @@ void fb(unsigned long f1, unsigned long f1o, unsigned long f2, unsigned long f2o
 	unsigned long tmp, tmp0;
 	unsigned long mx = 100000000;
 	int i;
-	
+
 	if (f1 == 1 && f2 == 1 && limit > 1)
 	{
 		printf("%lu, ", f1);
 		printf("%lu, ", f2 + f1);
 		fb(1, 0, 2, 0, limit - 2);
 	}
-	else if (limit == 1)
-	{
-		tmp = (f2 + f1) % mx;
-		tmp0 = f1o + f2o + (f1 + f2) / mx;
-		printf("%lu", tmp0);
-		i = num_len(mx) - 1 - num_len(tmp);
-		while (i > 0 && tmp0 > 0)
-		{
-			printf("0");
-			i--;
-		}
-		printf("%lu", tmp);
-	}
 	else
 	{
 		tmp = (f2 + f1) % mx;
 		tmp0 = f1o + f2o + (f1 + f2) / mx;
-		
+
 		if (tmp0 > 0)
 		{
 			printf("%lu", tmp0);
@@ -78,7 +67,8 @@ void fb(unsigned long f1, unsigned long f1o, unsigned long f2, unsigned long f2o
 			}
 			printf("%lu", tmp);
 		}
-		printf(", ");
+		if(limit != 1)
+			printf(", ");
 		fb(f2, f2o, (f1 + f2) % mx, f1o + f2o + (f1 + f2) / mx, limit - 1);
 	}
 }
