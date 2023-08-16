d90dfe434406d6975a47fafa6147e37f447f3b0e
SeifEleslam <seifgouda98@gmail.com>
2023-08-15 17:58:42 -0700

    test

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+nclude "main.h"
+
+/**
+ * jack_bauer - prints a 24 hours minute by minute 
+ */
+
+void jack_bauer(voud)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar(h / 10 + 48);
+			_putchar(h % 10 + 48);
+			_putchar(':');
+			_putchar(m / 10 + 48);
+			_putchar(h % 10 + 48);
+			_putchar('\n);
+		}
+	}
+}
+
