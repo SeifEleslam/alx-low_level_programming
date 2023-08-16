6b21c571150acc5ecae18e05a05577f0764937bf
SeifEleslam <seifgouda98@gmail.com>
2023-08-15 17:13:25 -0700

    task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - returns 1 if c is positive, -1 if negative, and 0 if 0
+ * @c: integer to be tested
+ * Return: 1 or 0 or -1
+ */
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		return (1);
+	}
+	else if (c < 0)
+	{
+		return (-1);
+	}
+	return (0);
+}
+
