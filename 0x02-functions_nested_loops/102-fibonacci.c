a0568232c69f88aeb96618e9dad5f6277074a35c
SeifEleslam <seifgouda98@gmail.com>
2023-08-16 01:24:58 -0700

    test

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,31 +1,39 @@
 #include "main.h"
 #include <stdio.h>
+
+
+/**
+ * fb - prints first n numbers of fibonacci seq
+ * Return: void
+ */
+
+void fb(long f1, long f2, int limit)
+{
+	if (f1 == 1 && f2 == 1 && limit > 1)
+	{
+		printf("%lu, ", f1);
+		printf("%lu, ", f2 + f1);
+		fb(1, 2, limit - 2);
+	}
+	else if (limit == 1)
+	{
+		printf("%lu", f1 + f2);
+	}
+	else
+	{
+		printf("%lu, ", f2 + f1);
+		fb(f2, f1 + f2, limit - 1);
+	}
+}
+
+
 /**
  * main - prints first 50 numbers of the fibonacci sequence
  * Return: 0
  */
 int main(void)
 {
-	int i, n;
-	long f1, f2, next;
-
-	f1 = 1;
-	f2 = 2;
-	n = 50;
-
-	for (i = 0; i < n; i++)
-	{
-		if (i == 49)
-		{
-			printf("%lu", f1);
-		} else
-		{
-			printf("%lu, ", f1);
-			next = f1 + f2;
-			f1 = f2;
-			f2 = next;
-		}
-	}
+	fb(1, 1, 50);
 	printf("\n");
 	return (0);
 }
