c7e790f8be7b02a9eeb30cad233b28e6c2f011d6
SeifEleslam <seifgouda98@gmail.com>
2023-08-17 16:27:20 -0700

    test

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle, which its line with len n.
+ * @size: the len of a line in square
+ */
+
+void print_square(int size)
+{
+	int i, l;
+
+	for (i = 0; i < size; i++)
+	{
+		for (l = 0; l < size; l++)
+		{
+			if (size - l <= i + 1)
+				_putchar('#');
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
+
