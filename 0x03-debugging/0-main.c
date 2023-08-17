088784679ce888f6fa55fbff52d4494c2db4c84e
SeifEleslam <seifgouda98@gmail.com>
2023-08-16 19:45:21 -0700

    test zero

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative or 0
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
