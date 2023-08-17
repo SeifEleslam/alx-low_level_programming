6716d90db2d5c19e13f51e91ab0c275c3552deed
SeifEleslam <seifgouda98@gmail.com>
2023-08-17 14:32:32 -0700

    test

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal line with len n.
+ * @n: the len of diagonal line
+ */
+
+void more_numbers(int n)
+{
+	int i, l;
+
+	for (i = 0; i < n; i++)
+	{
+		for (l = 0; l < i-1; l++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\');
+		_putchar('\n');
+	}
+       	if(n <= 0)
+		_putchar('\n');
+    
+}
