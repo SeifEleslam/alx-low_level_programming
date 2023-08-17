14e5e18442d728d95b81c0d8b3c066037d47987b
SeifEleslam <seifgouda98@gmail.com>
2023-08-17 15:21:26 -0700

    task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print Fuzz Buzz.
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 != 0 && i % 5 != 0)
+		{
+			printf("%i", i);
+		}
+		else
+		{
+			if (i % 3 == 0)
+				printf("Fizz");
+			if (i % 5 == 0)
+				printf("Buzz");
+		}
+		printf(" ");
+	}
+	printf("\n");
+}
+
