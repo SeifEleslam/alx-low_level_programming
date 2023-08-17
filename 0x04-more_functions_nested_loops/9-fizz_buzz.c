241ed70085196f5a6a0bee9487d2ef7bf24a8b1d
SeifEleslam <seifgouda98@gmail.com>
2023-08-17 15:56:49 -0700

    test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,9 +17,11 @@ int main(void)
 		}
 		else
 		{
-			if (i % 3 == 0)
+			if (i % 3 == 0 && i % 5 == 0)
+				printf("FizzBuzz");
+			else if (i % 3 == 0)
 				printf("Fizz");
-			if (i % 5 == 0)
+			else (i % 5 == 0)
 				printf("Buzz");
 		}
 		printf(" ");
