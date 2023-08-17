22b3449f96f6ac11721fe172db65f2ff949c1e62
SeifEleslam <seifgouda98@gmail.com>
2023-08-17 14:09:11 -0700

    test

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,12 +15,8 @@ void more_numbers(void)
 			if (i >= 10)
 			{
 				_putchar((i / 10) + 48);
-				_putchar((i % 10) + 48);
 			}
-			else
-			{
-				_putchar(i + 48);
-			}
+			_putchar((i % 10) + 48);
 		}
 		_putchar('\n');
 	}
