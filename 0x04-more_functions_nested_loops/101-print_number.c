46716d36c05a6728c4ba7a1789b44770d76b0657
SeifEleslam <seifgouda98@gmail.com>
2023-08-17 19:09:48 -0700

    test

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,31 +7,24 @@
 
 void print_number(int n)
 {
-	unsigned int tmp;
-
 	if (n < 0)
 	{
-		tmp = -n;
+		n = -n;
 		_putchar('-');
 	}
-	else
-	{
-		tmp = n;
-	}
 
-	if (tmp == 0)
+	if (n == 0)
 		_putchar(48);
 	else
 	{
-		if (tmp / 10 > 0)
+		if (n / 10 > 0)
 		{
-			tmp = tmp / 10;
-			print_number(tmp);
-			_putchar(tmp % 10 + 48);
+			print_number(n / 10);
+			_putchar(n % 10 + 48);
 		}
 		else if (n % 10 > 0)
 		{
-			_putchar(tmp + 48);
+			_putchar(n + 48);
 		}
 	}
 }
