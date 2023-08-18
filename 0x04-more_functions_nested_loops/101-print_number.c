3509b021cfa609ef8aa155d63dac7cefeb11bf56
SeifEleslam <seifgouda98@gmail.com>
2023-08-17 18:45:19 -0700

    test

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/*
+ * print_number - Print a number of any any digit
+ * @n : takes an input number
+ */
+
+void print_number(int n)
+{
+	int tmp2;
+
+	if (n == 0)
+		_putchar(48);
+	else
+	{
+		if (n / 10 > 0)
+		{
+			tmp2 = n / 10;
+			print_number(tmp2);
+			_putchar(n % 10 + 48);
+		}
+		else if (num % 10 > 0)
+		{
+			_putchar(n + 48);
+		}
+	}
+}
+
