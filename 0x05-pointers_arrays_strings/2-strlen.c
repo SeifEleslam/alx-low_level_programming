b7fb3d93a7ed1f002b35a8776a121c60dfedec53
SeifEleslam <seifgouda98@gmail.com>
2023-08-21 06:19:32 -0700

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,16 @@
 /**
  *_strlen - return the len of a str
  *@s : the first address of the string
+ *Return: the le of the str
  */
 
 int _strlen(char *s)
 {
 	int len;
-	
+
 	len = 0;
-	while(s[len] != '\0')
+	while (s[len] != '\0')
 		len++;
-	return len;
+	return (len);
 }
 
