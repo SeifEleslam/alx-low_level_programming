91ef006d554b509a52ffed2bc0c4d7f21a0db138
SeifEleslam <seifgouda98@gmail.com>
2023-08-21 06:35:34 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *_print_rev - print reverse of s followed by new line
+ *@s : the address of the str
+ */
+
+void print_rev(char *s)
+{
+	int i, len;
+
+	len = strlen(s);
+	for (i = len-1; i >=0; i--)
+		printf("%c", s[i])
+	printf("\n");
+}
+
