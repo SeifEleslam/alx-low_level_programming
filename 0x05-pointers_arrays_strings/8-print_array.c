660c2746a26fd18fc3a6fce11b639eb6c40c38f5
SeifEleslam <seifgouda98@gmail.com>
2023-08-21 12:17:12 -0700

    task8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,15 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if(a[i] != '\0')
+		if (a[i] != '\0')
+		{
+			if (i != 0)
+				printf(", ")
 			printf("%i", a[i]);
+		}
 		else
 			break;
 	}
-        printf("\n");
+	printf("\n");
 }
 
