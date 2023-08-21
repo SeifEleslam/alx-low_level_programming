ca224269a000787e5bcd4199ca017b4082069ada
SeifEleslam <seifgouda98@gmail.com>
2023-08-21 10:52:26 -0700

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *puts_half - print the second half of a str
+ *@str : the address of the str
+ */
+
+void puts_half(char *str)
+{
+	int i, len;
+
+	len = strlen(str);
+	for (i = len / 2; i < len; i ++)
+		printf("%c", str[i]);
+	printf("\n");
+}
+
