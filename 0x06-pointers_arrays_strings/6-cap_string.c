baea6edb545254e246d47a3f37123d22f3599863
SeifEleslam <seifgouda98@gmail.com>
2023-08-24 12:34:35 -0700

    task6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+/**
+ *string_toupper - concatenates two strings
+ *@str : the first address of the string
+ *Return : new string
+ */
+
+int string_includes(char *arr, char val)
+{
+	unsigned int i;
+
+	for (i = 0; arr[i] != '\0'; ++i)
+		if (arr[i] == val)
+			return (1);
+	return (0);
+}
+
+char *cap_string(char *str)
+{
+	int i, tmp;
+	char sperators;
+	
+	sperators[] = " \t,;.!?\"(){}\n";
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] <= 'z' && str[i] >= 'a' && string_includes(sperators, str[i - 1]))
+			str[i] = str[i] - ('a' - 'A');
+		i++;
+	}
+	return (str);
+}
+
