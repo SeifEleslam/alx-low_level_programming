e1d9e9c17f61a99cb441d9441c0a2b2042c3c0fe
SeifEleslam <seifgouda98@gmail.com>
2023-08-24 09:20:20 -0700

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+/**
+ *string_toupper - concatenates two strings
+ *@str : the first address of the string
+ *Return : new string
+ */
+
+char *string_toupper(char *str)
+{
+	int i, tmp;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] <= 'z' && str[i] >= 'a')
+			str[i] = str[i] - ('a' - 'A');
+		i++;
+	}
+	return (str);
+}
+
