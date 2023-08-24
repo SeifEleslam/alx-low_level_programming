45119261ed0226c74422caec7cc4fee3090c7366
SeifEleslam <seifgouda98@gmail.com>
2023-08-24 09:07:30 -0700

    task4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+/**
+ *reverse_array - concatenates two strings
+ *@a : the first address of the string
+ *@n : the second address of the other string
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, len, tmp;
+
+	i = 0;
+	while (i < n / 2)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp;
+		i++;
+	}
+}
+
