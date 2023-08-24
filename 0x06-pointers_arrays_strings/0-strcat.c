1d2ccfa837732cc0892df680d3cf16cfbbc71c33
SeifEleslam <seifgouda98@gmail.com>
2023-08-23 20:21:10 -0700

    task0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *_strcat - concatenates two strings
+ *@dest : the first address of the string
+ *@src : the second address of the other string
+ *Return: the dest followed by src
+ */
+
+int _strcat(char *dest, char *src)
+{
+	int i, n;
+
+	n = strlen(dest);
+	i = n;
+	while (src[i - n] != '\0' )
+	{
+		dest[i] = src[i-n];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
