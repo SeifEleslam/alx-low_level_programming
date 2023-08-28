ff4d64d85ed24fc1d1fdfaea1a1daf44dbc4c494
SeifEleslam <seifgouda98@gmail.com>
2023-08-28 14:30:58 -0700

    test

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - reset the parsing value to 98
+ * @s: first passing pointer
+ * @c: second passing pointer
+ * Return: pointer to s
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *r;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			r = s + i;
+			return (r);
+		}
+	}
+
+	return (NULL);
+}
+
