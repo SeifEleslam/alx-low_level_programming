d726f359c6defef8956fe8ec9de6a324c5fd9fce
SeifEleslam <seifgouda98@gmail.com>
2023-08-28 13:03:56 -0700

    test

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - reset the parsing value to 98
+ * @dest: first passing pointer
+ * @src: second passing pointer
+ * @n: number of repeated times
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i;
+	}
+
+	return (dest);
+}
+
