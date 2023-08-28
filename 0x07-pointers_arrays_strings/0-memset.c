97c0cb3936a1792843966e35f95ae89b311de7d7
SeifEleslam <seifgouda98@gmail.com>
2023-08-28 12:31:49 -0700

    task1

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - reset the parsing value to 98
+ * @s: first passing pointer
+ * @b: second passing pointer
+ * @n: number of repeated times
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
