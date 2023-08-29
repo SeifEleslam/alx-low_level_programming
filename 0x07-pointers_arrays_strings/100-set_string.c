c7dc6427b7b6324f80db4c55f3ffddccda043e03
SeifEleslam <seifgouda98@gmail.com>
2023-08-28 17:56:31 -0700

    test

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - reset the parsing value to 98
+ * @s: second passing pointer
+ * @to: number of repeated times
+ */
+
+void *set_string(char **s, char *to)
+{
+	*s = to;
+}
+
