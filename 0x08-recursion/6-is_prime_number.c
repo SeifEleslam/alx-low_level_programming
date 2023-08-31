c991a4e9064ed82b65e4c1b1c43c358d9b5bf1d7
SeifEleslam <seifgouda98@gmail.com>
2023-08-30 19:22:06 -0700

    test

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+
+/**
+ * prime_check - reset the parsing value to 98
+ * @n: second passing pointer
+ * @max: sda does something
+ * Return: sad
+ */
+
+int prime_check(int n, int max)
+{
+
+	if (max < (n / 2))
+	{
+		if (n % max == 0)
+			return (1);
+		else
+			return (prime_check(n, max + 1));
+	}
+	return (0);
+}
+
+/**
+ * is_prime_number - reset the parsing value to 98
+ * @n: second passing pointer
+ * Return: sad
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 0)
+		return (0);
+	if (n == 1 || n == 2)
+		return (1);
+	return (prime_check(n, 0));
+}
+
