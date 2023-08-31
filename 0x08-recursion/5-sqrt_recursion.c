13a57eb170d2df996ba841c6cbb71f8e8cba1b91
SeifEleslam <seifgouda98@gmail.com>
2023-08-30 18:56:36 -0700

    test

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,20 +6,35 @@
  * Return: sad
  */
 
+int square_root (int n, int max, int min);
+
 int _sqrt_recursion(int n)
 {
-	int sqrt_n;
+	return square_root(n, n, 0;
+}
 
-	if (n <= 0)
-		return (-1);
-	else if (n == 1 || n == 0)
-		return (n);
-	sqrt_n = _sqrt_recursion(n / 2);
-	if(sqrt_n * sqrt_n == n)	
-		return (sqrt_n);
-	else if (sqrt_n > 2)
-		return (sqrt_n);
-	else
-		return (-1);
+/**
+ * square_root - reset the parsing value to 98
+ * @n: second passing pointer
+ * @max: sda
+ * @min: dsadsa
+ * Return: sad
+ */
+
+int square_root (int n, int max, int min)
+{
+	int mid;
+
+	if (max >= min)
+	{
+		mid = (max - min) / 2;
+		if (mid * mid == n)
+			return mid;
+		else if (mid * mid < n)
+			return square_root(n, max, mid + 1);
+		else
+			return square_root(n, mid-1, min);
+	}
+	return (-1);
 }
 
