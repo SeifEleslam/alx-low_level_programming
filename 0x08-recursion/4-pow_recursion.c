a1113c8c3cdb8b887078116604674f232e7f0c40
SeifEleslam <seifgouda98@gmail.com>
2023-08-30 17:43:56 -0700

    test

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - reset the parsing value to 98
+ * @x: second passing pointer
+ * @y: power
+ * Return: sad
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+	else if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+	else
+		return (-1);
+		
+}
+
