1d3c1d3d21ad2690356e7088df2c086b7f0af3d7
SeifEleslam <seifgouda98@gmail.com>
2023-08-31 05:25:11 -0700

    test

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+
+/**
+ * wildcmp_rec - reset the parsing value to 98
+ * @s1: second passing pointer
+ * @s2: sda does something
+ * Return: sad
+ */
+
+char *wildcmp_rec(char *s1, char *s2)
+{
+	if (s2 == '\0' || s2 == '*' || s1 == '\0')
+		return (s1);
+	else if (s1 == s2)
+		return (wildcmp_rec(s1 + 1, s2 + 1));
+	else
+		return (wildcmp_rec(s1 + 1, s2));
+}
+
+/**
+ * ignore_star - reset the parsing value to 98
+ * @s: second passing pointer
+ * Return: sad
+ */
+
+chae *ignore_star(char *s)
+{
+	if (s != '*')
+		return (s);
+	else
+		return (ignore_star(s + 1);)
+}
+
+/**
+ * wildcmp - reset the parsing value to 98
+ * @s1: second passing pointer
+ * @s2: wildcmp
+ * Return: sad
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	char s;
+
+	if (s2 == '\0')
+		return (1);
+	else if (s1 == '\0')
+		return (0);
+	s = wildcmp_rec(s1, s2);
+	return (wildcmp(s, ignore_star(s2)))
+}
+
