d010611a673c78efafc5a01ed1b2cfd8c57a8e61
SeifEleslam <seifgouda98@gmail.com>
2023-08-30 20:06:13 -0700

    test

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+
+/**
+ * _strlen_recursion - reset the parsing value to 98
+ * @s: second passing pointer
+ * Return: val
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+		return (0);
+}
+
+/**
+ * palindrome_check - reset the parsing value to 98
+ * @len :int
+ * @s: second passing pointer
+ * Return: sad
+ */
+
+void palindrome_check(char *s, int len)
+{
+	if (_strlen_recursion(s) < =  1)
+		return (1);
+        if (s[0] == s[len-1])
+        {
+                palindorme_check(s + 1, len-1);
+        }
+        else
+                return (0);
+}
+
+/**
+ * is_palindrome - reset the parsing value to 98
+ * @s: second passing pointer
+ * Return: sada
+ */
+
+int is_palindrome(char *s)
+{
+	return (palindrome_check(s, _strlen_recursion(s)));
+}
+
