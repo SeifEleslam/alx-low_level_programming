9710567cd7ceee5d044f7f9998c4bf412db92c9a
SeifEleslam <seifgouda98@gmail.com>
2023-08-31 06:28:26 -0700

    test

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,14 +10,11 @@
 
 char *wildcmp_rec(char *s1, char *s2)
 {
-	char r[2];
-
 	if (*s2 == '\0' || *s2 == '*')
 		return (s1);
 	else if (*s1 == '\0')
 	{
-		r = '\0\0';
-		return r + 1;
+		return '\0';
 	}
 	else if (*s1 == *s2)
 		return (wildcmp_rec(s1 + 1, s2 + 1));
@@ -67,7 +64,7 @@ int wildcmp(char *s1, char *s2)
 
 	if (*s2 == '\0' && *s1 == '\0')
 		return (1);
-	else if (*s1 == '\0')
+	else if (s1 == '\0')
 		return (0);
 	s = wildcmp_rec(s1, s2);
 	if (*(s - 1) == '\0')
