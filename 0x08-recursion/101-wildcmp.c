c9e937b5f39d69c135ec3d7d34cdad10a1401ae1
SeifEleslam <seifgouda98@gmail.com>
2023-08-31 06:44:01 -0700

    test

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -64,11 +64,13 @@ int wildcmp(char *s1, char *s2)
 
 	if(!s1)
 		return (0);
-	else if (*s2 == '\0' && *s1 == '\0')
+	else if (*s2 == '\0' && (*s1 == '\0' || *(s2 - 1) == '*'))
 		return (1);
+	else if (*s2 == '\0' || *s1 == '\0')
+		return (0)
 
 	s = wildcmp_rec(s1, s2);
-	if (*(s - 1) == '\0')
+	if (!s)
 		ss = s2;
 	else
 		ss = ignore_star(reach_star(s2));
