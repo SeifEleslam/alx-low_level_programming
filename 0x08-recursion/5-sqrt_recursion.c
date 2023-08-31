b4427f9a86ec94040b2151ccce90ad250d940951
SeifEleslam <seifgouda98@gmail.com>
2023-08-30 18:24:05 -0700

    test

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - reset the parsing value to 98
+ * @n: second passing pointer
+ * Return: sad
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n <= 0)
+		return (-1);
+	else if (n == 1 || n == 0)
+		return (n)
+	sqrt_n = _sqrt_recursion(n / 2)
+	if(sqrt_n * sqrt_n == n)	
+		return (sqrtn);
+	else if (sqrt_n + 1 != 2)
+		return (sqrt_n + 1);
+	else
+		retunr (-1);
+}
+
