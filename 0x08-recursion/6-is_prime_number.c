61f3b40a8824fb1ebe18a2c5051b059cb78bb7fc
SeifEleslam <seifgouda98@gmail.com>
2023-08-30 19:25:44 -0700

    test

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -29,9 +29,9 @@ int prime_check(int n, int max)
 
 int is_prime_number(int n)
 {
-	if (n <= 0)
+	if (n <= 1)
 		return (0);
-	if (n == 1 || n == 2)
+	if (n == 2)
 		return (1);
 	return (prime_check(n, 2));
 }
