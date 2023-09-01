339a6a3a97fb601b5b18737a2896cdb737dd3138
SeifEleslam <seifgouda98@gmail.com>
2023-09-01 12:51:04 -0700

    test

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+include "main.h"
+
+/**
+ * main - main Entry
+ * @argc: int
+ * @argv: char**
+ * Return: int
+ */
+int main(int argc, char const __attribute__ ((unused)) *argv[])
+{
+	_putchar('0' + argc - 1);
+	_putchar('\n');
+	return (0);
+}
+
