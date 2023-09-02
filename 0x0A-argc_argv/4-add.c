be9df3c06d9e79877eede2495d8125251a579a80
SeifEleslam <seifgouda98@gmail.com>
2023-09-01 17:45:08 -0700

    test

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -27,10 +27,56 @@ int main(int argc, char *argv[])
 			_putchar('\n');
 			return (1);
 		}
-
 	}
 	print_number(sum);
 	_putchar('\n');
 	return (0);
 }
 
+/**
+ * _to_int - convert string to int
+ *
+ * @s: char*
+ * Return: int
+ */
+
+int _to_int(char *s)
+{
+	int i, r;
+
+	r = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			r *= 10;
+			r += s[i] - '0';
+		}
+		else if (s[i] == '-' && i == 0)
+			continue;
+		else
+			return ('\0');
+	}
+	if (s[0] == '-')
+		return (-1 * r);
+	return (r);
+}
+
+
+/**
+ * print_number - reverse the given string
+ * @n: array length
+ *
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -1 * n;
+	}
+	if (n > 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
+}
