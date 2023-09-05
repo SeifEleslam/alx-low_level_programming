9fb3f181fd476aa888bc8bee882ee93a9b0800d0
SeifEleslam <seifgouda98@gmail.com>
2023-09-05 05:27:36 -0700

    test

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - duplicate string and return duplicat
+ * @width: int
+ * @height: int
+ * Return: char*
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = (int **)malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < width; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (l = 0; l < i; l++)
+				free(grid[l]);
+			free(grid);
+			return (NULL);
+		}
+	}
+	return (grid);
+}
+
