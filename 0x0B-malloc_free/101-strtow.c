0fada02440bdc001da8c9b9e8d355d969861ceac
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-06 01:09:15 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,8 +9,9 @@
 
 char **strtow(char *str)
 {
-	int i, len;
-	char **newstr, *tmp;
+	int i, l, n, len;
+	char **newstr;
+
 	if (str == NULL || str == "")
 		return NULL;
 	len = 0;
@@ -20,8 +21,37 @@ char **strtow(char *str)
 	newStr = (char **) malloc(sizeof(char) * len + 1);
 	if (newStr == NULL)
 	       return NULL;
-	for (i = 0; str[i] != '\0'; i++)
+	i = 0, l = 0, n = 0;
+	newStr[i] = malloc
+	while (str[i] != '\0')
 	{
 		if (str[i] != ' ' && (i == 0 || str[i-1] == ' '))
-			for (l = i; str[l] != ' ' && str [l] != '\0'; l++
+		{
+			newStr[n] = malloc((word_len(str + i) + 1) * sizeof(char));
+			for (l = i; str[l] != ' ' && str [l] != '\0'; l++)
+			{
+				newStr[n][l] = str[i];
+				i++;
+			}
+			n++;
+		}
+		else
+			i++;
+	}
+	return (newStr);
+		
+}
+
+/**
+ * word_len - return the length of parsing param
+ * @s: string passing pointer
+ * Return: length of s
+ */
+int word_len(char *s)
+{
+	int i;
+
+	while (s[i] != ' ' && s[i] != '\0')
+		i++;
+	return (i);
 }
