6371972b1cdb4a630344ef3b37d3583318472b5f
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-06 03:19:16 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * _strdup - duplicate string and return duplicat
- * @str: char*
- * Return: char*
- */
-
 int word_len(char *s);
+
+/**
+ * strtow - duplicate string and return duplicat
+ * @str: char*
+ * Return: char**
+ */
 
 char **strtow(char *str)
 {
@@ -16,23 +16,23 @@ char **strtow(char *str)
 
 	len = 0;
 	for (i = 0; str[i] != '\0'; i++)
-		if (str[i] != ' ' && (i == 0 || str[i-1] == ' '))
-			len++;	
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+			len++;
 	if (str == NULL || len == 0)
 		return (NULL);
 
 	newStr = (char **) malloc(sizeof(char *) * len + 1);
 	if (newStr == NULL)
-	       return (NULL);
+		return (NULL);
 	i = 0, l = 0, n = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] != ' ' && (i == 0 || str[i-1] == ' '))
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 		{
 			newStr[n] = malloc((word_len(str + i) + 1) * sizeof(char));
 			if (newStr[n] == NULL)
 			{
-				for(i = 0; i < n; i++)
+				for (i = 0; i < n; i++)
 					free(newStr[i]);
 				free(newStr);
 				return (NULL);
