be3c5d78cd938bab4196ef563c21b6f6cfa39ea8
SeifEleslam <seifgouda98@gmail.com>
2023-09-07 14:16:03 -0700

    test

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int _strlen_recursion(char *s);
+/**
+ * string_nconcat - concatenation of s1 s2
+ * @s1: char *
+ * @s2: char *
+ * @n: int
+ * Return: char*
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int len1, len2, i;
+	char *newStr;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		len1++;
+	for (i = 0; s2[i] != '\0'; i++)
+		len2++;
+	if (len1 + len2 <= 0)
+		return (NULL);
+	if (len2 > n)
+	       len2 = n;
+	newStr = malloc((len1 + len2 + 1) * sizeof(char));
+	if (newStr = NULL)
+		return (NULL);
+	for(i = 0; s1[i] != '\0'; i++)
+		newStr[i] = s1[i];
+	for(;s2[i - len1] != '\0'; i++)
+		newStr[i] = s2[i-len1];
+	return (newStr);
+}
