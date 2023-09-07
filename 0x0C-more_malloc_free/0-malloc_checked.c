68f909cdf44521c620eae797dc28419052b2590e
SeifEleslam <seifgouda98@gmail.com>
2023-09-07 13:57:25 -0700

    test

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory
+ *
+ * @b: unsigned int
+ * Return: void*
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *output;
+
+	output = malloc(b);
+	if (output = NULL)
+		exit(98);
+	return output;
+}
+
