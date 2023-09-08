46da42a688977e19a063f1b0f63486c526c509fd
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-08 03:06:58 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _strlen(char *s);
+int _strlen_recursion(char *s);
 /**
  * string_nconcat - concatenation of s1 s2
  *
@@ -15,11 +15,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int len, _pos, _n, _glob_pos;
 	char *concat;
 
-	len = _strlen(s1) + n;
+	len = _strlen_recursion(s1) + n;
 	concat = malloc(len * sizeof(char) + 1);
 	_pos = 0, _n = 0, _glob_pos = 0;
 	if (concat == NULL)
+	{
 		return (NULL);
+	}
 
 	if (s1 != NULL)
 	{
@@ -34,8 +36,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 != NULL)
 	{
 		_n = n;
-		if (_n > _strlen(s2))
-			_n = _strlen(s2);
+		if (_n > _strlen_recursion(s2))
+		{
+			_n = _strlen_recursion(s2);
+		}
 		while (s2[_pos] && _pos < _n)
 		{
 			concat[_glob_pos] = s2[_pos];
@@ -48,16 +52,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 }
 
 /**
- * _strlen - return the length of parsing param
+ * _strlen_recursion - return the length of parsing param
  * @s: string passing pointer
  *
  * Return: length of s
  */
-int _strlen(char *s)
+int _strlen_recursion(char *s)
 {
 	if (s[0] == '\0' || s == NULL)
 	{
 		return (0);
 	}
 	return (1 + _strlen_recursion(&s[1]));
+
 }
