738ad63276e3ef633a413f4e18bba1cf1762fe07
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-08 07:21:39 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,6 +12,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *newptr;
 	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
 	if (ptr == NULL)
 	{
 		newptr = malloc(new_size);
@@ -24,8 +27,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size == old_size)
-		return (ptr);
+	
 	newptr = malloc(new_size);
 	if (newptr == NULL)
 		return (NULL);
