a1a4be8dad501dbe7f47e9985f9fe4ee92572577
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-08 03:12:30 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,20 +14,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int _tot_size;
 
 	if (nmemb == 0 || size == 0)
-	{
 		return (NULL);
-	}
+	
 	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
-	temp = ptr;
-	_tot_size = nmemb * size;
-	while (_tot_size--)
-	{
-		*temp++ = 0;
-	}
 	return (ptr);
 }
 
