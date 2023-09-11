3dd9365e27131bec7e537ca9992419e63b89c51a
SeifEleslam <seifgouda98@gmail.com>
2023-09-11 15:17:18 -0700

    test

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
 struct dog
 {
 	char *name;
