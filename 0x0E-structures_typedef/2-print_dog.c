cf5f1d9af7e5fed083a89cf6b7c0ff2172b59408
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-12 01:47:28 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void init_dog(struct dog *d)
 	if (d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
-		if(d.age)
+		if(d->age)
 			printf("Age: %f\n", d->age);
 		else
 			printf("Age: %s\n", "(nil)");
