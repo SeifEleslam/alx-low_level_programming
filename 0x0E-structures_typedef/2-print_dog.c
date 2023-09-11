2836124639afe65a8ca8b93b8968c18e930be8e3
SeifEleslam <seifgouda98@gmail.com>
2023-09-11 15:36:10 -0700

    test

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - check the code for
+ * @d: struct dog
+ */
+
+
+void init_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d.name ? d.name : "(nil)");
+		if(d.age)
+			printf("Age: %f\n", d.age);
+		else
+			printf("Age: %s\n", "(nil)");
+		printf("Owner: %s\n", d.name ? d.name : "(nil)");
+	}
+}
