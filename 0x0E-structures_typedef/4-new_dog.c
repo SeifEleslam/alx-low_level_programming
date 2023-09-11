a9d2a0b8f651d27635858cafa4d9236a650df1f0
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-12 02:44:15 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,13 +16,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (!d)
 		return NULL;
-	d->name = malloc(sizeof(char) * (strlen(name) + 1))
+	d->name = malloc(sizeof(char) * (strlen(name) + 1));
 	if (!d->name)
 	{
 		free(d);
 		return NULL;
 	}
-	d->owner = malloc(sizeof(char) * (strlen(owner) + 1))
+	d->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 	if (!d->owner)
 	{
 		free(d->name);
