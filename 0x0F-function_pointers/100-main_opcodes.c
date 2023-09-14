c22437d02b2e48d3c3b3d886cbe32f2c72acec9d
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-14 04:21:24 +0300

    Create 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Entry point
+ * @ac: n of params
+ * @av: params
+ * Return: 0 
+ */
+
+int main(int ac, char *av[])
+{
+	int i, n;
+
+	if (ac != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(av[1]);
+	if (n <= 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%02x", *((char *)main + i));
+		if (i < n - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
