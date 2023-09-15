fd051dfc62fe0951dee50efb0326e2b3758c00d7
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-15 09:22:19 +0300

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,6 @@
-#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 /**
  * print_strings - print strings
  * @separator: - char to sperate ints
@@ -10,15 +10,15 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-  char *str;
+	char *str;
 	va_list strs;
 
 	va_start(strs, n);
 	for (i = 0; i < n; i++)
-  {
-    str = v_arg(strs, char*);
+	{
+		str = v_arg(strs, char *);
 		printf("%s%s", str ? str : "(nil)", separator && i != n - 1 ? separator : "");
-  }
+	}
 	printf("\n");
 	va_end(strs);
 }
