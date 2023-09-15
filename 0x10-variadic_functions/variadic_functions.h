d5f33912c6d1be340611806a9bd5bba67c9b37b0
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-15 10:05:23 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,14 @@
-#ifndef VARIADICFUNCTIONSH
-#define VARIADICFUNCTIONSH
+#ifndef HEADERFILE
+#define HEADERFILE
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+typedef struct format {
+	void (*func)(va_list);
+	char type;
+}
 
 #endif
