269fdd2446a86f57769dd18dec2a44355e5da89e
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-16 03:16:29 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 /**
  * print_strings - print strings
  * @separator: - char to sperate ints
@@ -10,20 +11,77 @@
 void print_all(const char * const format, ...)
 {
 	int i;
-	char *str;
-	va_list strs;
+	int first;
+	print_func *funcs[26];
+	print_func *func;
+	va_list itms;
 
+	funcs['c' - 'a'] = print_char;
+	funcs['i' - 'a'] = print_int;
+	funcs['f' - 'a'] = print_float;
+	funcs['s' - 'a'] = print_string;
+
+	va_start(itms, format);
+	first = 0;
 	i = 0;
 	while (format[i] != '\0')
-		i++;
-	
-	va_start(strs, n);
-	for (i = 0; i < n; i++)
 	{
-		str = va_arg(strs, char *);
-		printf("%s%s", str ? str : "(nil)",
-			separator && i != n - 1 ? separator : "");
+		func = funcs[format[i] - 'a'];
+		if (func)
+		{
+			if (first > 0)
+				printf(", ");
+			func(itms);
+			first = 1;
+		}
+		i++;
 	}
 	printf("\n");
 	va_end(strs);
+}
+
+
+/**
+ * print_char - prints a char
+ * @itms: va_list 
+ * Return: void
+ */
+void print_char(va_list list)
+{
+	printf("%c", (char) va_arg(list, int));
+}
+
+/**
+ * print_int - prints an int 
+ * @itms: va_list
+ * Return: void
+ */
+void print_int(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
+/**
+ * print_float - prints a float
+ * @itms: va_list
+ * Return: void
+ */
+void print_float(va_list list)
+{
+	printf("%f", (float) va_arg(list, double));
+}
+
+/**
+ * print_string - prints a string
+ * @itms: va_list
+ * Return: void
+ */
+void print_string(va_list list)
+{
+	char *str = va_arg(list, char*);
+
+	if (str)
+		printf("%s", str);
+	else
+		printf("(nil)");
 }
