c0580f449b971607820d839c1fc7415914847852
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-22 02:47:48 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,8 @@
  * @head: head
  * Return: new node
  */
-__attribute__((constructor)) void first_func()
+__attribute__((constructor)) void first_func(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
