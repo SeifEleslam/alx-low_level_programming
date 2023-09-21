95efa907e202a0259dfe95f45de70ab6b05c90d3
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-22 02:30:40 +0300

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * add_node_end - check the code
+ * @str: str
+ * @head: head
+ * Return: new node
+ */
+__attribute__((constructor)) void *add_node_end(list_t **head, const char *str)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
