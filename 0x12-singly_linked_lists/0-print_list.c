765f395276698c06d32232627e44e64b964a0776
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-21 18:25:50 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,21 +8,21 @@
 
 size_t print_list(const list_t *h)
 {
-    char err[5] = "(nil)";
-    int i;
-
-    i = 0;
-    while (h)
-    {
-        _putchar('[');
-        print_num(h->str ? h->len : 0);
-        _putchar(']');
-        _putchar(' ');
-        print_str(h->str ? h->str : &err, h->str ? h->len : 5);
-        _putchar('\n');
-        h = h->next;
-        i++;
-    }
-    return (i);
+	char err[5] = "(nil)";
+	int i;
+	
+	i = 0;
+	while (h)
+	{
+		_putchar('[');
+		print_num(h->str ? h->len : 0);
+		_putchar(']');
+		_putchar(' ');
+		print_str(h->str ? h->str : &err, h->str ? h->len : 5);
+		_putchar('\n');
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
 
