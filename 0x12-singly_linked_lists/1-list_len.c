0e2436d33fadb8821f6697809ad1d74fb0afc1d8
SeifEleslam <seifgouda98@gmail.com>
2023-09-21 08:57:52 -0700

    test

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - des
+ * @h: list
+ * Return: size_t
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i;
+	
+	i = 0;
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return i;
+}
+
