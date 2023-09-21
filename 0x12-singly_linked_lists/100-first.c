5560d937cd12db5348488b499e1f1df168f7a6ca
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-22 02:58:07 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,7 @@
 #include "lists.h"
 /**
- * add_node_end - check the code
- * @str: str
- * @head: head
- * Return: new node
+ * first_func - check the code
  */
-
-void __attribute__((constructor)) first_func(void);
 void first_func(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
