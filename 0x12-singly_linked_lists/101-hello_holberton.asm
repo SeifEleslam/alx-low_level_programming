9136fd1f6b19375fef16f933a653383a69848ff1
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-22 03:53:10 +0300

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -9,12 +9,12 @@ section .text
 main:
    push rbp
 
-   mov rdi,fmt
-   mov rsi,msg
-   call printf
-
-   pop rbp
-
-   mov rax,0
-   ret
+    mov rdi,fmt
+    mov rsi,msg
+    mov rax,0
+    call printf
+    
+    pop rbp
+    mov rax,0
+    ret
 
