11b957bc9267e7f9b92bc5d4a3e0f21aa4e6dabf
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-26 18:48:25 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,10 +3,11 @@
  * free_listint2 - check the code
  * @head: head
  */
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
-	if (!head)
+	if (!(*head))
 		return;
-	free_listint2(head->next);
-	free(head);
+	free_listint2((*head)->next);
+	free(*head);
+	*head = NULL;
 }
