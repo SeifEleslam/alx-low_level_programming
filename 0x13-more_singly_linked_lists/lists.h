63734e9d420f6da1322e95c3a0b8bf43ef05ff18
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-26 07:35:50 +0300

    Create lists.h

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char);
+size_t print_listint(const listint_t *h);
+
+#endif
