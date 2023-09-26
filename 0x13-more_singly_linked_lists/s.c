f49472a3e1f714f066747e4c7d0ba7cf3b2cf89a
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-27 01:44:30 +0300

    Create s.c

---
+++ 0x13-more_singly_linked_lists/s.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * pop_listint - check the code
+ * @head: head
+ * Return: int
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *prvH;
+	int val;
+
+	if (!head || !(*head))
+		return (0);
+	prvH = *head;
+	val = (int)prvH->n;
+	head = &((*head)->next)
+	free(prvH);
+	return (val);
+}
