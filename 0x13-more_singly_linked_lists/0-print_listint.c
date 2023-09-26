e6cee900bffe31216f6d894368b6b382960aa932
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-26 07:34:27 +0300

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - print linked list
+ * @h: list
+ * Return: list length
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+	
+	i = 0;
+	while (h)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
