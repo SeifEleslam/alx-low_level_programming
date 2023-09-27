ac10256bb302353993a070b9f7708983815a43a3
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-27 05:16:12 +0300

    Create s.c

---
+++ 0x13-more_singly_linked_lists/s.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * find_listint_loop - find a loop
+ * @head: head
+ * Return: node loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *curr, *point;
+	
+	if (!head)
+		return (NULL);
+	for (curr = head; curr->next != NULL; curr = curr->next)
+	{
+		if (curr == curr->next)
+			return (curr);
+		for (point = head; point != curr; point = point->next)
+			if (point == curr->next)
+				return (curr);
+	}
+	return (NULL);
+}
