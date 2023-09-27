0a7d65e98ac48d6df4b634462bd883cd52435f16
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-27 04:22:42 +0300

    Create 100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * reverse_listint - check the code
+ * @head: head
+ * Return: listint_t
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *curr, *parent, *next;
+
+	if (!head || !(*head))
+	return (NULL);
+	parent = *head;
+	curr = parent->next;
+	parent->next = NULL;
+	while (curr)
+	{
+		next = curr->next;
+		curr->next = parent;
+		parent = curr;
+		curr = next;
+	}
+	*head = parent;
+	return parent;
+}
