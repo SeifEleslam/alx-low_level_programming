74e1f6069b39116f3d02a3f1774f1c7ecca653ea
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-27 03:57:37 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,6 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		{
 			node = curr->next;
 			curr->next = node->next;
+			free(node);
 			return (1);
 		}
 		curr = curr->next;
