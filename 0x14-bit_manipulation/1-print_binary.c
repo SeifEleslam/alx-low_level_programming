99df880dbc30b235bcac9e55fb4d8faf86fb9674
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-29 02:54:11 +0300

    Create 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+/**
+ * print_binary - des
+ * @n: int
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (n > 0)
+	{
+		_putchar((n | 0) + '0');
+		n >>= 1;
+	}
+}
