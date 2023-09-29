e6d7c621ff8317628ebda554d1dfc76d99509a66
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-29 03:53:45 +0300

    Create 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+/**
+ * set_bit - des
+ * @n: int
+ * @index: int
+ * Return: int
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index > (sizeof(*n) * 8))
+		return (-1);
+	i = 1;
+	*n &= (~(i << index));
+	return (1);
+}
