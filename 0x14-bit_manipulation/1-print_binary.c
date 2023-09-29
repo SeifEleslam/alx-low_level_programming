1c50615db32b1777b667670ecb05f94b5d07e642
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-29 03:23:23 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,24 @@
 #include "main.h"
 /**
+ * print_binary_rec - des
+ * @n: int
+ */
+void print_binary_rec(unsigned long int n)
+{
+	if ((n >> 1) > 0)
+		print_binary_rec(n >> 1);
+	_putchar((n & 1) + '0');
+}
+
+/**
  * print_binary - des
  * @n: int
  */
 void print_binary(unsigned long int n)
 {
 	if (n == 0)
-	{
 		_putchar('0');
-		return;
-	}
-	while (n > 0)
-	{
-		_putchar((n & 1) + '0');
-		n >>= 1;
-	}
+	else
+		print_binary_rec(n);
 }
+
