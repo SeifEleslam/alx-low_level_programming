2c0a5a7b2c697fec2a82a2200c54b14245240e02
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-29 03:30:21 +0300

    Create 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,12 @@
+/**
+ * get_bit - des
+ * @n: long
+ * @index: uint
+ * Return: int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	return ((n >> index) & 1);
+}
