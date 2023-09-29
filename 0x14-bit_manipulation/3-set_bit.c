75e47c2a6c420f70590f5664cd2bdf8137cfa37e
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-29 03:42:20 +0300

    Update 3-set_bit.c'

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,11 +6,11 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned long int i;
+	unsigned long int i;
 
-    if (index > (sizeof(*n) * 8))
-        return (-1);
-    i = 1;
-    *n |= (i << index);
-    return (1);
+	if (index > (sizeof(*n) * 8))
+		return (-1);
+	i = 1;
+	*n |= (i << index);
+	return (1);
 }
