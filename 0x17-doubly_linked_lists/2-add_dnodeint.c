c72e4ddecf183718cfc822d5e15a923e59951f95
SeifEleslam <seifgouda98@gmail.com>
2023-11-09 16:39:22 +0200

    test

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,10 +15,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->prev = NULL;
 	new_node->next = *head;
 	if (*head)
+	{
+		new_node->prev = (*head)->prev;
 		(*head)->prev = new_node;
+	}
 	*head = new_node;
 
 	return (new_node);
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - add node to end of d linked list
+ * @h: head of the list
+ * @idx: index to add in
+ * @n: value to add
+ * Return: new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	int i;
+
+	for (i = 0; *h; *h = (*h)->next)
+}
