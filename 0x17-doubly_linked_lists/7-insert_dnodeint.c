35863e5083f1ac36cfdb5b0951d5d98a403129df
SeifEleslam <seifgouda98@gmail.com>
2023-11-09 20:23:16 +0200

    test

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,23 +11,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i;
 	dlistint_t *curr;
-	dlistint_t *new_node;
 
-	if (!h)
-		return NULL;
 	if (idx == 0)
-	{
-		new_node = add_dnodeint(h, n);
-		return (new_node);
-	}
-	for (i = 0, curr = *h; curr; curr = (curr)->next, i++)
-	{
-		if (i == idx)
-		{
-			new_node = add_dnodeint(&curr, n);
-			break;
-		}
-	}
+		return (add_dnodeint(h, n));
 
-	return (new_node);
+	for (i = 0, curr = *h; curr; curr = (curr)->next, i++)
+		if (i == idx)
+			return (add_dnodeint(&curr, n));
+
+	return (NULL);
 }
