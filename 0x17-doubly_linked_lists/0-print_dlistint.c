76589d8a9390810b05dc8d0a6dfcad2360d663f0
SeifEleslam <seifgouda98@gmail.com>
2023-11-09 14:53:50 +0200

    test

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,7 +5,8 @@ size_t print_dlistint(const dlistint_t *h)
     dlistint_t *head;
     size_t len;
 
-    for (head = (dlistint_t *)h, len = 0; head; head = head->next)
+    len = 0;
+    for (head = (dlistint_t *)h; head; head = head->next)
         printf("%d\n", head->n), len++;
     return (len);
 }
