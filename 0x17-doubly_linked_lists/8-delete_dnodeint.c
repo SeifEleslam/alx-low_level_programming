b8744e52b4c88d1c4800a53aed6ab4c6eaf36254
SeifEleslam <seifgouda98@gmail.com>
2023-11-09 21:22:29 +0200

    test

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - add node to end of d linked list
+ * @head: head of the list
+ * @index: index to add in
+ * Return: 1 | -1
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *curr;
+
+	if (!*head)
+		return (-1);
+	(*head)->prev = NULL;
+	if (index == 0)
+	{
+		curr = *head;
+		*head = (*head)->next;
+		free(curr);
+		return (1);
+	}
+	for (i = 0, curr = *head; curr->next; curr = (curr)->next, i++)
+	{
+		if (i == index)
+		{
+			curr->prev->next = curr->next;
+			curr->next->prev = curr->prev;
+			free(curr);
+			return (1);
+		}
+	}
+	if (index == i)
+	{
+		curr->prev->next = NULL;
+		free(curr);
+		return (1);
+	}
+	return (-1);
+}
