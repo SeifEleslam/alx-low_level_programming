144a0204470b9eface1f4cdbfd2d729363d73738
SeifEleslam <seifgouda98@gmail.com>
2023-11-09 14:52:15 +0200

    task1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,9 @@
 size_t print_dlistint(const dlistint_t *h)
 {
     dlistint_t *head;
+    size_t len;
 
-    for (head = (dlistint_t *)h; head; head = head->next)
-        printf("%d\n", head->n);
+    for (head = (dlistint_t *)h, len = 0; head; head = head->next)
+        printf("%d\n", head->n), len++;
+    return (len);
 }
