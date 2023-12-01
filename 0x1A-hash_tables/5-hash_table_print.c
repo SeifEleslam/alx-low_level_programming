60a108d6d13f20ff0dce81ceb0558c1c62bf1cec
SeifEleslam <seifgouda98@gmail.com>
2023-12-01 23:04:43 +0200

    test

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,11 +11,9 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *curr;
 
-	if (ht == NULL)
-		return;
 	printf("{");
 
-	for (i = 0; i < ht->size; i++)
+	for (i = 0; ht && i < ht->size; i++)
 	{
 		if (ht->array[i])
 		{
