f63a27443f30a6fb6fff7c1eae6a1311a91130b4
SeifEleslam <seifgouda98@gmail.com>
2023-12-01 16:13:49 +0200

    tas0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(*ht));
+	if (!ht)
+		return (NULL);
+	ht->size = size;
+}
