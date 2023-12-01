d5956a66c91f3d9931b9383717241fffd02e3914
SeifEleslam <seifgouda98@gmail.com>
2023-12-01 16:19:45 +0200

    tas0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,12 +1,21 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - Create Hash table
+ * @size: The size of the array
+ * Return: Ref to hashtable
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
+	hash_node_t **arr;
 
 	ht = malloc(sizeof(*ht));
 	if (!ht)
 		return (NULL);
-	ht->size = size;
+	arr = malloc(sizeof(*arr));
+	if (!arr)
+		return (NULL);
+	ht->array = arr;
 	return (ht);
 }
