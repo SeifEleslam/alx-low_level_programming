daf581a746c2367ccb8a08ffb2fce5c9d8b9ccef
SeifEleslam <seifgouda98@gmail.com>
2024-01-25 04:04:11 +0200

    interview question

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,22 +2,30 @@
 """Island Perimeter"""
 
 
-def getNeighborsNum(grid, row, col):
+def getPerimeter(grid, x, y, done=dict()):
     """Get num of neighbors"""
-    g
+    if y >= len(grid) or y < 0 or x >= len(grid[y]) or x < 0:
+        return (0, 0)
+    if f'{x},{y}' in done:
+        return (0, 1)
+    if grid[y][x] == 1:
+        done['%d,%d' % (x, y)] = True
+        sum = 0
+        n1 = getPerimeter(grid, y, x+1)
+        n2 = getPerimeter(grid, y+1, x)
+        n3 = getPerimeter(grid, y-1, x)
+        n4 = getPerimeter(grid, y, x-1)
+        return (4 + n1[0] + n2[0] + n3[0] + n4[0] - n1[1] - n2[1] - n3[1] - n4[1], 1)
+
+    return (0, 0)
 
 
 def island_perimeter(grid):
     """Return the perimeter of an island in a grid."""
     if not grid:
         return 0
-    rows = len(grid)
-    cols = len(grid[0])
-    for row in range(rows-1):
-        for col in range(cols-1):
+    for row in range(len(grid)-1):
+        for col in range(len(grid[0])-1):
             if grid[row][col] == 1:
-                neighbors = getNeighborsNum(grid, row, col)
-    print(rows[1])
-
-
-island_perimeter()
+                return getPerimeter(grid, col, row)[0]
+    return 0
