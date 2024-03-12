d7c0e441652096f27d1d5c484c510e2969f0f3ed
SeifEleslam <seifgouda98@gmail.com>
2024-03-12 14:01:36 +0200

    searching linear

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+/**
+ * linear_search - Entry point
+ * @array: the array we search in
+ * @value:  the value we are searching for
+ * @size: the size of array
+ * Return: -1 or the idx
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (size <= 0 || array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%i] = [%i]\n",
+			   i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
