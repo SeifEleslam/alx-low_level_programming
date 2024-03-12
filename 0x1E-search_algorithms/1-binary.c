c951e21c9750e6f5844fbdc78dc7f480153548ab
SeifEleslam <seifgouda98@gmail.com>
2024-03-12 15:20:04 +0200

    binary search, need sort

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -36,7 +36,7 @@ int binary_search(int *array, size_t size, int value)
 		else if (array[mid] > value)
 			size = mid;
 		else
-			array = array + mid + 1, ref += mid,
+			array = array + mid + 1, ref += mid + 1,
 			size = size - mid - 1;
 	}
 	return (-1);
