920993ae7de6ffba1a667514ad464df461727314
SeifEleslam <seifgouda98@gmail.com>
2024-03-14 00:26:19 +0200

    skip

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - Jump to it
+ * @list: the array we search in
+ * @value:  the value we are searching for
+ * Return: -1 or the idx
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *start = list, *curr = start;
+
+	if (list == NULL)
+		return (NULL);
+	for (curr = start->express; curr; curr = curr->express)
+	{
+		printf("Value checked at index [%lu] = [%i]\n", curr->index, curr->n);
+		if (curr->n >= value || !curr->next)
+			break;
+		start = curr;
+		if (!curr->express)
+		{
+			while (curr->next)
+				curr = curr->next;
+			printf("Value checked at index [%lu] = [%i]\n", curr->index, curr->n);
+			break;
+		}
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n",
+		   start->index, curr->index);
+	for (; start != curr; start = start->next)
+	{
+		printf("Value checked at index [%lu] = [%i]\n",
+			   start->index, start->n);
+		if (start->n == value)
+			return (start);
+	}
+	return (NULL);
+}
